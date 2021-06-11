5bd65498861139d6c87e93f3826c4073aeca2c4f
killermazaga2 <48801682+killermazaga2@users.noreply.github.com>
2021-06-12 03:26:57 +0530

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,6 +1,11 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from tkinter import *
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import webbrowser
 import pyttsx3
 import speech_recognition as sr
 import threading
@@ -12,28 +17,70 @@ engine.setProperty('voice', voices[1].id)
 def talk(talk):
     engine.say(talk)
     engine.runAndWait()
-
+talk("Welcome user")
 bot = ChatBot("alexa")
 
-convo=[
+greetings=[
     'Hello',
     'hi there!',
+    'Hello',
+    'Hello there',
+    'Hi',
+    'Hi there',
+    'Hi',
+    'Hello there',
+    'Good morning',
+    'Good morning to you too',
+    'Good evening',
+    'Good evening to you too',
+]
+
+information=[
     'What is your name?',
-    'My name is alexa, I am created by Tinesh',
-    'How are you ?',
-    'I am doing great these days',
-    'Thank you',
-    'In which city you live ?',
-    'I live in Chennai',
-    'In which language you talk?',
-    'I mostly talk in english'
+    'My name is Alexa',
+    'Who developed you?',
+    'I was developed by Tinesh',
+    'Who is your owner?',
+    'My owner is Tinesh',
+    'Who is your friend?'
+    'My friend is Tinesh',
+    'Where are you from',
+    "I'm from Tinesh's heart",
+    'Where are you from?',
+    "I'm from Chennai",
+    "Where are you from?",
+    "I'm from Chennai too",
+    "What is your owner doing?",
+    'My owner is exploring his life'
+]
+
+ending=[
+    'Okay, bye bye',
+    'Bye bye',
+    'Bye bye to you too',
+    'Yes, bye',
+    'Good night',
+    'Good night to you too',
+    'Take care',
+    'Yes, you too',
+    'You too take care',
+    'Peace out',
+    'Peace out bro',
+    'Ta ta',
+    'Ta ta bye bye',
+    "I'm going to sleep",
+    "Go to sleep, then",
+    "I want to sleep",
+    "Okay good night then"
 ]
 
 trainer = ListTrainer(bot)
 
 # #Train with trainers
 
-trainer.train(convo)
+trainer.train(greetings)
+trainer.train(information)
+trainer.train(ending)
 
 
 # print('Tak to alexa')
@@ -53,19 +100,55 @@ main.title("Alexa the ChatBot")
 def take_query():
     listener = sr.Recognizer()
     listener.pause_threshold=1
-    print("Alexa is listening ...")
     with sr.Microphone() as source:
+        print("Alexa is listening ...")
         try:
             voice = listener.listen(source)
             query = listener.recognize_google(voice, language='eng-in')
+            query = query.lower()
             print(query)
             textF.delete(0, END)
-            textF.insert(0, query)
-            ask_from_alexa()
+            textF.insert(0, query) 
+            if 'time' in query:
+                time = datetime.datetime.now().strftime('%H:%M %p')
+                msgs.insert(END, "You: "+ query)
+                msgs.insert(END, "alexa: The current time is  "+ time)
+                talk('The current time is: '+ time)
+                textF.delete(0,END)
+                msgs.yview(END)
+            elif 'play' in query:
+                song = query.replace('play', '')
+                pywhatkit.playonyt(song)
+                msgs.insert(END, "You: "+ query)
+                msgs.insert(END, "alexa: Playing  "+ song)
+                talk('Playing'+ song)
+                textF.delete(0,END)
+                msgs.yview(END)
+            elif 'wikipedia' in query:
+                person= query.replace('wikipedia','')
+                info= wikipedia.summary(person, 1)
+                msgs.insert(END, "You: "+ query)
+                msgs.insert(END, "alexa: "+ info)
+                talk(info)
+                textF.delete(0,END)
+                msgs.yview(END)
+            elif 'joke' in query:
+                py_joke =pyjokes.get_joke 
+                msgs.insert(END, "You: "+ query)
+                msgs.insert(END, "alexa: "+ py_joke)
+                talk(py_joke)
+            elif 'search' in query:
+                search= query.replace('search', '')
+                url= 'https://google.com/search?q=' + search
+                webbrowser.get().open(url)
+                msgs.insert(END, "You: "+ query)
+                msgs.insert(END, "alexa: Here is what I have found "+ search)
+                talk("Here is what I have found")
+            else:
+                ask_from_alexa()    
         except Exception as e:
             print(e)
             print("Not recognized")
-
 def ask_from_alexa():
     query = textF.get()
     answer_from_bot= bot.get_response(query)
