d96942d22251796489067e973144e655fb46d2d7
killermazaga2 <48801682+killermazaga2@users.noreply.github.com>
2021-06-11 17:32:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+import webbrowser
+import time
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -10,30 +16,55 @@ def talk(talk):
     engine.say(talk)
     engine.runAndWait()
 
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('Say Something...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:                        
-                print(command)
-                command = command.replace('alexa', '')
-            else:
-                print('Say Alexa')
-                talk('Say Alexa')
-                
-    except:
-        pass
-    return command
+def take_query():
+    with sr.Microphone() as source:        
+        print('Say Something...')
+        talk('Say Something...')
+        voice = listener.listen(source)
+        try:
+            query = listener.recognize_google(voice)
+            query = query.lower()                        
+            print(query)
+            query = query.replace('alexa', '')
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that')
+            pass
+        return query
 
 def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
+    query = take_query()
+    print(query)
+    if 'play' in query:
+        song = query.replace('play', '')
         talk('playing' + song )
-        print('playing')
+        pywhatkit.playonyt(song)
+    elif ('time') in query:
+        time = datetime.datetime.now().strftime('%H:%M %p')
+        print('The current time is: '+ time)
+        talk('The current time is: '+ time)
+    elif 'wikipedia' in query:
+        person= query.replace('wikipedia','')
+        info= wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'joke' in query:
+        talk(pyjokes.get_joke)
+    elif 'search' in query:
+        search= query.replace('search', '')
+        url= 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('Here is what I found ' + search)
+        talk('Here is what I found')
+    elif ('Find' and 'Location') or ('Location' or 'Located') in query:
+        location= query.replace('Location', '')
+        url= 'https://google.nl/maps/place/' + location + '/&amp;'
+        webbrowser.get().open(url)
+        print('Here is the location ' + search)
+        talk('Here is the location')
+    else:
+        print('Say it again')
+        talk('Say it again')
 
-run_alexa()
+time.sleep(1)
+while 1:
+    run_alexa()
